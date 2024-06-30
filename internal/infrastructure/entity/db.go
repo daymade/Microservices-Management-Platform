c6cf660fa6a88bd5fa9975499738b67c5bb147be
daymade <daymadev89@gmail.com>
2024-06-30 21:53:42 +0800

    perf(postgres): add index on name and desciptions

--- internal/infrastructure/entity/db.go
+++ internal/infrastructure/entity/db.go
@@ -5,28 +5,28 @@ import (
 )
 
 type User struct {
-	ID        uint64 `gorm:"primaryKey"`
-	Username  string `gorm:"unique;not null"`
-	Email     string `gorm:"unique;not null"`
-	CreatedAt time.Time
+	ID        uint64    `gorm:"primaryKey"`
+	Username  string    `gorm:"unique;not null"`
+	Email     string    `gorm:"unique;not null"`
+	CreatedAt time.Time `gorm:"index"`
 }
 
 type Service struct {
 	ID          uint64 `gorm:"primaryKey"`
-	Name        string `gorm:"not null"`
+	Name        string `gorm:"not null;index"`
 	Description string
 	OwnerID     uint64
-	Owner       User `gorm:"foreignKey:OwnerID"`
-	Versions    []Version
-	CreatedAt   time.Time
+	Owner       User      `gorm:"foreignKey:OwnerID"`
+	Versions    []Version `gorm:"foreignKey:ServiceID"`
+	CreatedAt   time.Time `gorm:"index"`
 	UpdatedAt   time.Time
 }
 
 type Version struct {
-	ID          uint64 `gorm:"primaryKey"`
-	ServiceID   uint64
+	ID          uint64  `gorm:"primaryKey"`
+	ServiceID   uint64  `gorm:"index"`
 	Service     Service `gorm:"foreignKey:ServiceID"`
 	Number      string  `gorm:"not null"`
 	Description string
-	CreatedAt   time.Time
+	CreatedAt   time.Time `gorm:"index"`
 }
